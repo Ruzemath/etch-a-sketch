e5bdf134d273bf54ad7f720c7b42500f5cda3a2a
Ruzemath <82734181+Ruzemath@users.noreply.github.com>
2023-08-10 23:47:11 -0700

    Added css style to website contents like text, buttons, input values, etc.

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
 let color = 'black';
 let choice = 'none'; 
 let colorChoice = document.getElementById('colorChoice');
-colorChoice.oninput = (e) => newColor(e.target.value);  
+colorChoice.oninput = (e) => newColor(e.target.value);
+ 
 let draw = true;
-document.querySelector('body').addEventListener('click', (e) => {
+document.querySelector('body').addEventListener('click', (e) => 
+{
     if(e.target.tagName != 'BUTTON' && e.target.tagName != 'INPUT')
     {
         draw = !draw;
@@ -11,21 +13,21 @@ document.querySelector('body').addEventListener('click', (e) => {
             document.querySelector('.draw').textContent = "Draw: Enabled";
         else
             document.querySelector('.draw').textContent = "Draw: Disabled";
-    }
+    }   
 });
 createGrid(16);
 
 function createGrid(size)
 {
-    let grid = document.querySelector('.grid');
-    let totalGrid = grid.querySelectorAll('div');
+    const grid = document.querySelector('.grid');
+    const totalGrid = grid.querySelectorAll('div');
     totalGrid.forEach(div => div.remove());
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
     for(let i = 0; i < size * size; i++)
     {
-        let index = document.createElement('div');
+        const index = document.createElement('div');
         index.addEventListener('mouseover', colorGrid);
         index.style.backgroundColor = 'white';
         grid.insertAdjacentElement('beforeend',index);
@@ -35,33 +37,27 @@ function createGrid(size)
 function colorGrid()
 {
     if (choice === "rainbow" && draw)
-    {
         this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-    }
     else if(choice === "color" && draw)
-    {
         this.style.backgroundColor = color;
-    }
     else if(choice === "eraser" && draw)
-    {
         this.style.backgroundColor = 'white';
-    }
 }
 
 function resetGrid()
 {
-    let grid = document.querySelector('.grid');
-    let totalGrid = grid.querySelectorAll('div');
+    const grid = document.querySelector('.grid');
+    const totalGrid = grid.querySelectorAll('div');
     totalGrid.forEach(div => div.style.backgroundColor = 'white');
 }
 
 function newSize(newSize)
 {
-    if(newSize >= 2 && newSize <= 64)
+    if(newSize >= 2 && newSize <= 128)
     {
         document.querySelector('.error').style.display = 'none';
         createGrid(newSize);
-        let sizeValue = document.getElementById('sizeValue');
+        const sizeValue = document.getElementById('sizeValue');
         sizeValue.innerText = `${newSize} x ${newSize}`;
     }
     else
@@ -71,6 +67,7 @@ function newSize(newSize)
 function newColor(newColor)
 {
     color = newColor;
+    colorChoice.style.background = newColor;
 }
 
 function newChoice(newChoice)
